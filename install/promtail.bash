7449354b52d75d6cb761947ec69c7edc0dfa3a10
Pedro Santos <peterpedrods@gmail.com>
2024-12-21 13:32:45 -0300

    Update promtail.bash

--- install/promtail.bash
+++ install/promtail.bash
@@ -3,7 +3,6 @@
 # Define Promtail version
 PROMTAIL_VERSION="3.3.0"
 DEFAULT_PROMTAIL_BIN_FILENAME="promtail-linux-amd64"
-PROMTAIL_BIN_URL="https://github.com/grafana/loki/releases/download/v$PROMTAIL_VERSION/$PROMTAIL_BIN_FILENAME.zip"
 INSTALL_DIR="/usr/local/bin"
 CONFIG_DIR="/etc/promtail"
 SERVICE_FILE="/etc/systemd/system/promtail.service"
@@ -12,28 +11,29 @@ DEFAULT_LOKI_URL="http://localhost:3100/loki/api/v1"
 LOKI_URL=$(whiptail --inputbox "Enter loki api url:" 10 60 "$DEFAULT_LOKI_URL" 3>&1 1>&2 2>&3)
 PROMTAIL_BIN_FILENAME=$(whiptail --inputbox "Enter loki binary name zip without extension:" 10 60 "$DEFAULT_PROMTAIL_BIN_FILENAME" 3>&1 1>&2 2>&3)
 
+PROMTAIL_BIN_URL="https://github.com/grafana/loki/releases/download/v${PROMTAIL_VERSION}/${DEFAULT_PROMTAIL_BIN_FILENAME}.zip"
+
 # Update system and install dependencies
 echo "Updating system and installing dependencies..."
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y curl unzip
+sudo apt update && sudo apt install -y unzip
 
 # Step 1: Download Promtail binary
-echo "Downloading Promtail binary..."
-curl -LO $PROMTAIL_BIN_URL
+echo "Downloading Promtail binary... $PROMTAIL_BIN_URL"
+wget -q -O "${PROMTAIL_BIN_FILENAME}.zip" $PROMTAIL_BIN_URL
 
 # Step 2: Unzip Promtail binary
 echo "Unzipping Promtail binary..."
-unzip "$PROMTAIL_BIN_FILENAME.zip"
+unzip $PROMTAIL_BIN_FILENAME || exit 1
 
 # Step 3: Move the Promtail binary to the install directory
 echo "Installing Promtail binary to $INSTALL_DIR"
-sudo mv $PROMTAIL_BIN_FILENAME $INSTALL_DIR/promtail
+sudo mv $PROMTAIL_BIN_FILENAME $INSTALL_DIR/promtail || exit 1
 sudo chmod +x $INSTALL_DIR/promtail
 
 # Step 4: Remove the downloaded zip file and unnecessary packages to save space
 echo "Cleaning up downloaded files and unnecessary packages..."
 rm -f "$PROMTAIL_BIN_FILENAME.zip"
-sudo apt-get remove --purge -y curl unzip
+sudo apt-get remove --purge -y unzip
 sudo apt-get autoremove -y
 sudo apt-get clean
 
@@ -83,10 +83,6 @@ echo "Reloading systemd, enabling, and starting Promtail service..."
 sudo systemctl daemon-reload
 sudo systemctl enable promtail
 sudo systemctl start promtail
-
-# Step 9: Verify if Promtail is running
-echo "Checking if Promtail is running..."
-sudo systemctl status promtail
 
 # Completion message
 echo "Promtail installation complete. It is now running as a service."
